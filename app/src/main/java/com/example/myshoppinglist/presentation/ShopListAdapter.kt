b8e558a6d40f9455c93f613287cc1ad3c02e2ff4
Sergey <s01denisov@gmail.com>
2022-02-27 13:56:49 +0300

    Реализация ShopItemViewHolder

---
+++ app/src/main/java/com/example/myshoppinglist/presentation/ShopListAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.myshoppinglist.presentation
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myshoppinglist.R
+import com.example.myshoppinglist.domain.ShopItem
+
+class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
+
+    val list = listOf<ShopItem>()
+    //Как создавать View
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_shop_disabled, parent, false)
+        return ShopItemViewHolder(view)
+    }
+    //Как вставлять значения внутри View
+    override fun onBindViewHolder(viewHolder: ShopItemViewHolder, position: Int) {
+        val shopItem = list[position]
+        viewHolder.tvName.text = shopItem.name
+        viewHolder.tvCount.text = shopItem.count.toString()
+        viewHolder.view.setOnLongClickListener {
+            true
+        }
+    }
+    //Возвращает число элементов
+    override fun getItemCount(): Int {
+        return list.size
+    }
+    //Класс для хранения ShopItemView
+    class ShopItemViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+        val tvName = view.findViewById<TextView>(R.id.tv_name)
+        val tvCount = view.findViewById<TextView>(R.id.tv_count)
+    }
+}
